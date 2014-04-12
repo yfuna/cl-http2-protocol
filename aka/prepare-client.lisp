49d34701166c46bba2b49bbd7d10b1d983aa511f
Yosuke Funahashi <yosuke@funahashi.cc>
2014-04-12 19:17:42 +0900

    Utilitiy files

---
+++ aka/prepare-client.lisp
@@ -0,0 +1,11 @@
+(ql:quickload :swank)
+(ql:quickload :alexandria)
+(ql:quickload :babel)
+(ql:quickload :puri)
+(ql:quickload :usocket)
+(ql:quickload :cl+ssl)
+
+(load "/Users/yfuna/dev/cl-http2-protocol/cl-http2-protocol.asd")
+(require :cl-http2-protocol)
+(in-package :http2-example)
+;;(example-client "https://localhost:8888/")
