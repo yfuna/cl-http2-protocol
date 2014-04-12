0c958ec49cc93fda5cda29c75c306138bfa7a0b3
Yosuke Funahashi <yfuna@squeeze>
2014-04-12 20:19:17 +0900

    Debian version

--- aka/start-server.lisp
+++ aka/start-server.lisp
@@ -5,7 +5,7 @@
 (ql:quickload :usocket)
 (ql:quickload :cl+ssl)
 
-(load "/Users/yfuna/dev/cl-http2-protocol/cl-http2-protocol.asd")
+(load "/home/yfuna/dev/cl-http2-protocol/cl-http2-protocol.asd")
 (require :cl-http2-protocol)
 (in-package :http2-example)
 (example-server :secure t :port 8888)
